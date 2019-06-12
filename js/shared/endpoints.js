911993c2c644b4045c905e11c346b2fa239de01c
ivanbalic <balicivan2@gmail.com>
2019-06-12 11:49:17 +0200

    endpoint fix

--- js/shared/endpoints.js
+++ js/shared/endpoints.js
@@ -1,2 +1,2 @@
-export const SHOWS_ENDPOINT = "http://api.tvmaze.com/shows";
-export const SEARCH_SHOWS_ENDPOINT = "http://api.tvmaze.com/search/shows";
+export const SHOWS_ENDPOINT = "https://api.tvmaze.com/shows";
+export const SEARCH_SHOWS_ENDPOINT = "https://api.tvmaze.com/search/shows";
